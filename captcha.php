ad4e62fe5d76fbba587d51177100cdc0cb72c6fe
Samuel Ricci <trumpetersamr@gmail.com>
2015-06-29 17:04:41 -0600

    Initial Code
    Created the initial captcha code based off of the tutorial from
    http://www.ivebe.com/blog/simple-php-captcha-tutorial.html

---
+++ captcha.php
@@ -0,0 +1,27 @@
+<?php
+
+session_start();
+
+header("Expires: Tue, 01 Jan 2013 00:00:00 GMT");
+header("Last-modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-checked=0", false);
+header("Pragma: no-cache");
+
+$chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+$randomString = '';
+
+for ( $i = 0; $i < 6; $i++ ) {
+    $randomString .= $chars[rand( 0, strlen($chars)-1 )];
+}
+
+$_SESSION['captcha'] = strtolower();
+
+//Create and insert captcha background here
+// $im = @imagecreatfrompng( "" );
+
+imagettftext($im, 30, 0, 10, 38, imagecolorallocate ($im, 0, 0, 0), 'larabiefont.tff', $randomString);
+
+header( "Content-type: image/png");
+imagepng($im, NULL, 0);
+imagedestroy($im);
